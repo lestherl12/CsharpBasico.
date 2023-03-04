a0519fff490d88e2a10053d607cfa97755e6943a
lestherl12 <106944199+lestherl12@users.noreply.github.com>
2023-03-03 22:26:07 -0600

    Add files via upload

---
+++ entradasalida/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace fundamentos 
+{
+    internal class SntradaSalida
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+
+            Console.Write("Escribe tu nombre: ");
+            nombre = Console.ReadLine();
+            Console.WriteLine("Hola " + nombre);
+            Console.Read();
+        }
+    }
+}
