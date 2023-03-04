a9b5874785b95fd337d151885d09263df6dfaf1b
lestherl12 <106944199+lestherl12@users.noreply.github.com>
2023-03-03 22:24:54 -0600

    Add files via upload

---
+++ Condiciones/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Sentencias 
+{
+    internal class SentenciaIF 
+    {
+        static void Main(string[] args)
+        {
+            int numeroEntero = int.Parse(Console.ReadLine());
+
+            if (numeroEntero == 7)
+            {
+                Console.WriteLine("Numero es 7.");
+            }
+            else
+            {
+                if (numeroEntero < 0)
+                {
+                    Console.WriteLine("numeroEntero es negativo");
+                }
+                else
+                {
+                    Console.WriteLine("numeroEntero no es ninguno de los casos programados");
+                }
+            }
+        }
+    }
+}
