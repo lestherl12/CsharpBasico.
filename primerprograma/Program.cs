76a9126cd90ad2e3378be83baef3d2c58ef53c6d
lestherl12 <106944199+lestherl12@users.noreply.github.com>
2023-03-03 22:23:10 -0600

    Add files via upload

---
+++ primerprograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace programa1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo Facil, haz Click!");
+        }
+    }
+}
---
+++ using/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Sentencias
+{
+    internal class SentenciasUsing
+    {
+        public static void Main(string[] args)
+        {
+            string s;
+
+            using (TextWriter w = File.CreateText("log.txt"))
+            {
+                w.WriteLine("linea uno");
+                w.WriteLine("linea dos");
+            }
+
+            using (TextReader r = File.OpenText("log.txt"))
+            {
+                while ((s = r.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
